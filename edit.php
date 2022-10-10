f6bfd5f035d1c05ee19a7a69ebbc36bcdc0d993f
EduardoScopel <espostiglione@ucs.br>
2022-10-10 16:34:58 -0300

    ok

--- edit.php
+++ edit.php
@@ -45,7 +45,7 @@ if (isset($_POST['submit'])) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Ficha do Aluno</title>
-  <link rel="stylesheet" href="styleformulario.css" />
+  <link rel="stylesheet" href="style.formulario.css" />
 </head>
 
 <body>
