be53f8dce182654bc03a22f845148a46b07254dd
EduardoScopel <espostiglione@ucs.br>
2022-10-13 14:18:45 -0300

    dsa

--- edit.php
+++ edit.php
@@ -1,54 +1,42 @@
 <?php
+    include_once('config.php');
 
-if (!empty($_GET['idaluno'])) {
-
-  include_once('config.php');
-
-  $idaluno = $_GET['idaluno'];
-
-  $sqlSelect = "SELECT * FROM aluno WHERE idaluno=$idaluno";
-
-  $result = $mysql->query($sqlSelect);
-
-  if($result->num_rows > 0)
-  {
-    while($user_Data = mysqli_fetch_assoc($result))
+    if(!empty($_GET['idaluno']))
     {
-    $nome = $_POST['nome'];
-    $rg = $_POST['rg'];
-    $cpf = $_POST['cpf'];
-    $datanasc = $_POST['datadenascimento'];
-    $nomemae = $_POST['nomedamae'];
-    $cpfmae = $_POST['cpfdamae'];
-    $nomepai = $_POST['nomedopai'];
-    $cpfpai = $_POST['cpfdopai'];
-    $celular = $_POST['telefone'];
-    $endereco = $_POST['endereço'];
-    $email = $_POST['email'];
-    $telefonefixo = $_POST['telefonefixo'];
-    $nomeresponsavel = $_POST['nomedoresponsavel'];
-    $cpfresponsavel = $_POST['cpfdoresponsavel'];
-    $alergia = $_POST['alergia'];
-    $pagamentos = $_POST['pagamentos'];
+        $idaluno = $_GET['idaluno'];
+        $sqlSelect = "SELECT * FROM aluno WHERE idaluno=$idaluno";
+        $result = $mysql->query($sqlSelect);
+        if($result->num_rows > 0)
+        {
+            while($user_data = mysqli_fetch_assoc($result))
+            {
+              $nome = $user_data['nome'];
+              $rg = $user_data['rg'];
+              $cpf = $user_data['cpf'];
+              $datanasc = $user_data['datadenascimento'];
+              $nomemae = $user_data['nomedamae'];
+              $cpfmae = $user_data['cpfdamae'];
+              $nomepai = $user_data['nomedopai'];
+              $cpfpai = $user_data['cpfdopai'];
+              $celular = $user_data['telefone'];
+              $endereco = $user_data['endereço'];
+              $email = $user_data['email'];
+              $telefonefixo = $user_data['telefonefixo'];
+              $nomeresponsavel = $user_data['nomedoresponsavel'];
+              $cpfresponsavel = $user_data['cpfdoresponsavel'];
+              $alergia = $user_data['alergia'];
+              $pagamentos = $user_data['pagamentos'];
+            }
+        }
+        else
+        {
+            header('Location: consulta.php');
+        }
     }
-    print_r($nome);
-  }
-  else
-  {
-    header('Location: consulta.php');
-  }
-
-}
-
-    session_start();
-    if((!isset($_SESSION['usuario']) == true) and (!isset($_SESSION['senha']) == true))
+    else
     {
-        unset($_SESSION['usuario']);
-        unset($_SESSION['senha']);
-        header('Location: login.php');
+        header('Location: consulta.php');
     }
-    $logado = $_SESSION['usuario'];
-
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -71,94 +59,94 @@ if (!empty($_GET['idaluno'])) {
         <legend><strong>Ficha do Aluno</strong></legend>
         <div class="inputBox">
           <label for="nome" class="labelinput">Nome Completo do Aluno:</label>
-          <input type="text" name="nome" id="nome" class="inputUser" required />
+          <input type="text" name="nome" id="nome" class="inputUser" value=<?php echo $nome;?> required />
         </div>
         <br />
         <div class="inputBox">
           <label for="datadenascimento" class="labelinput">Data de Nascimento:</label>
-          <input type="date" name="datadenascimento" id="datadenascimento" class="inputdatadenascimento" required />
+          <input type="date" name="datadenascimento" id="datadenascimento" class="inputdatadenascimento" value=<?php echo $nome;?> required />
         </div>
         <br />
         <div class="inputBox">
           <label for="rg" class="labelinput">Nº do RG:</label>
-          <input type="text" name="rg" \ pattern="\d{2}\.\d{3}\.\d{3}-\d{1}" \ title="Digite um RG no formato: xx.xxx.xxx-x" class="inputUser" required />
+          <input type="text" name="rg" \ pattern="\d{2}\.\d{3}\.\d{3}-\d{1}" \ title="Digite um RG no formato: xx.xxx.xxx-x" class="inputUser" value=<?php echo $nome;?> required />
         </div>
         <br />
         <div class="inputBox">
           <label for="cpf" class="labelinput">Nº do CPF:</label>
-          <input type="text" name="cpf" \ pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" \ title="Digite um CPF no formato: xxx.xxx.xxx-xx" class="inputUser" required />
+          <input type="text" name="cpf" \ pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" \ title="Digite um CPF no formato: xxx.xxx.xxx-xx" class="inputUser" value=<?php echo $nome;?> required />
         </div>
         <br />
         <div class="inputBox">
           <label for="nomedamae" class="labelinput">Nome da Mãe:</label>
-          <input type="text" name="nomedamae" id="nomedamae" class="inputUser" required />
+          <input type="text" name="nomedamae" id="nomedamae" class="inputUser" value=<?php echo $nome;?> required />
         </div>
         <br />
         <div class="inputBox">
           <label for="cpfdamae" class="labelinput">CPF da Mãe:</label>
-          <input type="text" name="cpfdamae" \ pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" \ title="Digite um CPF no formato: xxx.xxx.xxx-xx" class="inputUser" required />
+          <input type="text" name="cpfdamae" \ pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" \ title="Digite um CPF no formato: xxx.xxx.xxx-xx" class="inputUser" value=<?php echo $nome;?> required />
         </div>
         <br />
         <div class="inputBox">
           <label for="nomedopai" class="labelinput">Nome do Pai:</label>
-          <input type="text" name="nomedopai" id="nomedopai" class="inputUser" required />
+          <input type="text" name="nomedopai" id="nomedopai" class="inputUser" value=<?php echo $nome;?> required />
         </div>
         <br />
         <div class="inputBox">
           <label for="cpfdopai" class="labelinput">CPF do Pai:</label>
-          <input type="text" name="cpfdopai" \ pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" \ title="Digite um CPF no formato: xxx.xxx.xxx-xx" class="inputUser" required />
+          <input type="text" name="cpfdopai" \ pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" \ title="Digite um CPF no formato: xxx.xxx.xxx-xx" class="inputUser" value=<?php echo $nome;?> required />
           <br />
           <br />
           <div class="inputBox">
             <label for="telefone" class="labelinput">Celular para Contato:</label>
-            <input type="tel" name="telefone" id="telefone" class="inputUser" pattern="\d{5}-\d{4}" title="Digite um Telefone no formato: xxxxx-xxxx" required />
+            <input type="tel" name="telefone" id="telefone" class="inputUser" pattern="\d{5}-\d{4}" title="Digite um Telefone no formato: xxxxx-xxxx" value=<?php echo $nome;?> required />
           </div>
           <br />
           <div class="inputBox">
             <label for="endereço" class="labelinput">Endereço Completo:</label>
-            <input type="text" name="endereço" id="endereço" class="inputUser" required />
+            <input type="text" name="endereço" id="endereço" class="inputUser" value=<?php echo $nome;?> required />
           </div>
           <br />
           <div class="inputBox">
             <label for="email" class="labelinput">E-mail:</label>
-            <input type="email" name="email" id="email" class="inputUser" required />
+            <input type="email" name="email" id="email" class="inputUser" value=<?php echo $nome;?> required />
           </div>
           <br />
           <div class="inputBox">
             <label for="telefonefixo" class="labelinput">Fone Residencial:</label>
-            <input type="tel" name="telefonefixo" id="telefonefixo" class="inputUser" pattern="\d{4}-\d{4}" title="Digite um Telefone no formato: (+xx)xxxx-xxxx" />
+            <input type="tel" name="telefonefixo" id="telefonefixo" class="inputUser" pattern="\d{4}-\d{4}" title="Digite um Telefone no formato: (+xx)xxxx-xxxx" value=<?php echo $nome;?> />
           </div>
           <br /><br />
           <div class="inputBox">
             <label for="nomedoresponsavel" class="labelinput">Nome do Responsável:</label>
-            <input type="text" name="nomedoresponsavel" id="nomedoresponsavel" class="inputUser" />
+            <input type="text" name="nomedoresponsavel" id="nomedoresponsavel" class="inputUser" value=<?php echo $nome;?> />
           </div>
           <br />
           <div class="inputBox">
             <label for="cpfdoresponsavel" class="labelinput">CPF do Responsável:</label>
-            <input type="text" name="cpfdoresponsavel" \ pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" \ title="Digite um CPF no formato: xxx.xxx.xxx-xx" class="inputUser" required />
+            <input type="text" name="cpfdoresponsavel" \ pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" \ title="Digite um CPF no formato: xxx.xxx.xxx-xx" class="inputUser" value=<?php echo $nome;?> required />
           </div>
           <br /><br /><br />
           <p>Possui Alergia a algum Medicamento?</p>
           <br />
           <div class="alergia1">
             <label for="opc3" class="labelinput">Sim,Qual?(Caso não tenha não é necessario responder.)</label>
-            <input type="text" name="alergia" id="alergia" />
+            <input type="text" name="alergia" id="alergia" value=<?php echo $nome;?> />
             <br /><br>
             <br />
           </div>
           <br />
           <p>Opcão de Pagamento</p>
           <br />
-          <input type="radio" name="pagamentos" id="Carnê" value="Carnê" required />
+          <input type="radio" name="pagamentos" id="Carnê" value="Carnê" <?php echo ($pagamentos == 'Carnê') ? 'checked' : '';?> required />
           <label for="opc1.5">Carnê</label>
-          <input type="radio" name="pagamentos" id="Pix" value="Pix" required />
+          <input type="radio" name="pagamentos" id="Pix" value="Pix" <?php echo ($pagamentos == 'Pix') ? 'checked' : '';?> required />
           <label for="opc2.5">Pix</label>
-          <input type="radio" name="pagamentos" id="Boleto" value="Boleto" required />
+          <input type="radio" name="pagamentos" id="Boleto" value="Boleto" <?php echo ($pagamentos == 'Boleto') ? 'checked' : '';?> required />
           <label for="opc3.5">Boleto</label>
-          <input type="radio" name="pagamentos" id="Dinheiro" value="Dinheiro" required />
+          <input type="radio" name="pagamentos" id="Dinheiro" value="Dinheiro" <?php echo ($pagamentos == 'Dinheiro') ? 'checked' : '';?> required />
           <label for="opc4">Dinheiro</label>
-          <input type="radio" name="pagamentos" id="Cartão" value="Cartão" required />
+          <input type="radio" name="pagamentos" id="Cartão" value="Cartão" <?php echo ($pagamentos == 'Cartão') ? 'checked' : '';?> required />
           <label for="opc5">Cartão</label>
             <br>
             <button class="submit" name="submit" type="submit" id="submit">
@@ -170,4 +158,3 @@ if (!empty($_GET['idaluno'])) {
       <a class="Voltar" href='consulta.php'>Voltar</a>
     </body>
   </html>
-}
